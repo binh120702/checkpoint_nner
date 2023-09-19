a2db9f09820554aca3c71dd3392bb48a95d46d7c
binh120702 <thaibinh120702@gmail.com>
2023-09-19 18:37:06 +0000

    update infer file

--- infer_multiple_files.py
+++ infer_multiple_files.py
@@ -1,18 +1,19 @@
 import os 
 import subprocess
 
-DATA_FOLDER = '/workspace/checkpoint_nner/data/sagri/spec'
+DATA_FOLDER = '/workspace/nner/checkpoint_nner/data/sagri/spec'
+CHECKPOINT_PATH = '/workspace/nner/checkpoint_nner/epoch34.pth'
 
 for i in os.listdir(DATA_FOLDER):
     
     data = os.path.join(DATA_FOLDER, i)
     CONFIG = f"DATA_PATH = '{data}'  \
-        \nCHECKPOINT = '/workspace/epoch34.pth' \
+        \nCHECKPOINT = '{CHECKPOINT_PATH}' \
         \nTMP_DATA = 'tmp_data'   # default \
         \nTMP_INFER = 'tmp_infer' # default \
         \nFINAL = 'final_result' # default \
         \nFINAL_NAME = '{i}' \
-        \nINFER_BATCH_SIZE = 20 "
+        \nINFER_BATCH_SIZE = 200 "
         
     f = open("pipeline_setup.py", "w")
     f.write(CONFIG)
